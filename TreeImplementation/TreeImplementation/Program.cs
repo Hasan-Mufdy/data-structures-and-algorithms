232ae8ad65e3d698bf1e63c8b9e6ec6d2932a959
HasanMufdy <96997047+HasanMufdy@users.noreply.github.com>
2023-08-09 01:28:44 +0300

    fizz-buzz tree implementation is added

--- TreeImplementation/TreeImplementation/Program.cs
+++ TreeImplementation/TreeImplementation/Program.cs
@@ -58,6 +58,14 @@ internal class Program
         Console.WriteLine("maximum value in the tree is: " + binaryTree.MaximumValue());
 
         //////////////////////////////////////////////////////////////////////////////////////////////
+        ///
+        Console.WriteLine("tree-fizz-buzz:");
+
+
+        BinaryTree<string> fizzBuzzTree = binaryTree.FizzBuzzTree();
+
+        Console.WriteLine("FizzBuzz Tree: " + string.Join(" - ", fizzBuzzTree.BFS(fizzBuzzTree.Root)));
+
     }
 }
 
@@ -86,7 +94,67 @@ public class BinaryTree<T>
 
     /// ///////////////////////////////////////////////////////////////////////////////////
 
+    public BinaryTree<string> FizzBuzzTree()
+    {
+        BinaryTree<string> fizzBuzzTree = new BinaryTree<string>();
 
+        if (Root == null)
+        {
+            return fizzBuzzTree;
+        }
+
+        Queue<Node<T>> originalNodes = new Queue<Node<T>>();
+        Queue<Node<string>> fizzBuzzNodes = new Queue<Node<string>>();
+
+        originalNodes.Enqueue(Root);
+        fizzBuzzNodes.Enqueue(new Node<string>(FizzBuzzValue(Root.Value)));
+
+        fizzBuzzTree.Root = fizzBuzzNodes.Peek();
+
+        while (originalNodes.Count > 0)
+        {
+            Node<T> originalNode = originalNodes.Dequeue();
+            Node<string> fizzBuzzNode = fizzBuzzNodes.Dequeue();
+
+            if (originalNode.Left != null)
+            {
+                originalNodes.Enqueue(originalNode.Left);
+                fizzBuzzNode.Left = new Node<string>(FizzBuzzValue(originalNode.Left.Value));
+                fizzBuzzNodes.Enqueue(fizzBuzzNode.Left);
+            }
+
+            if (originalNode.Right != null)
+            {
+                originalNodes.Enqueue(originalNode.Right);
+                fizzBuzzNode.Right = new Node<string>(FizzBuzzValue(originalNode.Right.Value));
+                fizzBuzzNodes.Enqueue(fizzBuzzNode.Right);
+            }
+        }
+
+        return fizzBuzzTree;
+    }
+
+    private string FizzBuzzValue(dynamic value)
+    {
+        if (value % 3 == 0 && value % 5 == 0)
+        {
+            return "FizzBuzz";
+        }
+        else if (value % 3 == 0)
+        {
+            return "Fizz";
+        }
+        else if (value % 5 == 0)
+        {
+            return "Buzz";
+        }
+        else
+        {
+            return value.ToString();
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////
 
 
     //public T MaximumValue()
